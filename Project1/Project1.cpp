90d98f92dc193f42a5c121293355120d56485fa9
Sashka Rybin <rybin2003@inbox.ru>
2024-10-22 19:35:23 +0300

    метод определения процессов, настройка таймера обновления таблицы

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -1,11 +1,75 @@
 #include "Project1.h"
+#include <QTimer>
+#include <QFileDialog>
+#include <QTableWidgetItem>
+#include <QFile>
+#include <QFileInfo>
+#include <QDir>
+#include <windows.h>
+#include <tlhelp32.h>
+
+//путь к файлу
+const QString csvFilePath = QDir::currentPath() + "/process_list.csv";
 
 Project1::Project1(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    //настройки таймера
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &Project1::checkProcesses);
+    timer->start(1000); //интервал в 1с
 }
 
 Project1::~Project1()
 {}
 
+//метод определения активности процессов
+void Project1::checkProcesses() {
+    //снимок всех процессов в системе
+    HANDLE hProcessesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    PROCESSENTRY32 pe32; //инф-я о процессе
+    pe32.dwSize = sizeof(PROCESSENTRY32); //размер структуры
+
+    //установка статуса по умолочанию для всех процессов
+    for (int i = 0; i < processList.size(); i++) {
+        processList[1].isActive = false;
+    }
+
+    //перебор всех процессов
+    if (Process32First(hProcessesSnapshot, &pe32)) {
+        do {
+            QString currentProcessName = QString::fromWCharArray(pe32.szExeFile);
+
+            for (int i = 0; i < processList.size(); i++) {
+                if (processList[i].name == currentProcessName) {
+                    processList[i].isActive = true; //процесс найден -> активен
+                }
+            }
+        } while (Process32Next(hProcessesSnapshot, &pe32));
+    }
+    CloseHandle(hProcessesSnapshot);
+
+    //обновление статуса в таблице
+    for (int i = 0; i < processList.size(); ++i) {
+        for (int j = 0; j < ui.tableWidget->rowCount(); ++j) {
+            if (ui.tableWidget->item(i, 0)->text() == processList[i].name) {
+                ui.tableWidget->item(j, 2)->setText(processList[i].isActive ? "Active" : "Inactive");
+
+            }
+        }
+        //проверка закрытых процессов
+        if (!processList[i].isActive) {
+            for (int j = 0; j < ui.tableWidget->rowCount(); ++j) {
+                if (ui.tableWidget->item(j, 0)->text() == processList[i].name) {
+                    ui.tableWidget->item(j, 2)->setText("Inactive");
+                }
+            }
+        }
+
+        //сброс статуса для следующей проверки
+        processList[i].isActive = false;
+    }
+}
--- Project1/Project1.h
+++ Project1/Project1.h
@@ -2,6 +2,14 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_Project1.h"
+#include <QTimer>
+
+//структура информации о процессе
+struct ProcessInfo {
+    QString name;
+    QString path;
+    bool isActive;
+};
 
 class Project1 : public QMainWindow
 {
@@ -11,6 +19,14 @@ public:
     Project1(QWidget *parent = nullptr);
     ~Project1();
 
+private slots:
+    void checkProcesses(); //проверка состояния процессов
+    void on_actionOpen(); //файл -> открыть...
+    void on_actionClear(); //таблица -> очистить...
+
 private:
     Ui::Project1Class ui;
+    QTimer* timer;
+    QList<ProcessInfo> processList;
+
 };
